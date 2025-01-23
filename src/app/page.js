f39177806d915950ac2040709cad0d769159a9a5
sayed725 <ssayed72533@gmail.com>
2025-01-23 21:07:35 +0600

    added show blogs to home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,38 @@
-import Image from "next/image";
 
-export default function Home() {
+import BlogCard from "@/components/Blogcard";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
+
+export default async function Home() {
+
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const blogs = await res.json();
+
+
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="min-h-screen">
       
       
-      <h2 className="text-3xl font-bold">Welcome to Next.js!</h2>
+      <div className="pb-6">
+        <h1 className="text-3xl md:text-4xl lg:text-5xl mt-3 text-center">Explore Blogs</h1>
+      </div>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+        {blogs.slice(0, 12).map((blog) => (
+          <BlogCard key={blog.id} blog={blog} />
+        ))}
+      </div>
+      {/* view more button */}
+      <div className="w-1/2 md:w-1/3 lg:w-1/4 mx-auto mt-8">
+        <Link href={"/blogs"}>
+          <Button className="w-full">View More</Button>
+        </Link>
+      </div>
+
+
+
     </div>
   );
 }
